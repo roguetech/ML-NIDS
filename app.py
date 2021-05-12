1cfa32d33b25efa332183ee5ed8a5c3b205e3708
Patrick Roughan <padraig.oruchain@gmail.com>
2021-05-12 19:15:41 +0100

    adding app and pcap files

--- app.py
+++ app.py
@@ -1,8 +1,6 @@
 from flask import Flask, request
 import pickle as p
-import sklearn
 import json
-import numpy as np
 import pandas as pd
 import logging
 
@@ -10,28 +8,24 @@ app = Flask(__name__)
 
 logging.basicConfig(level=logging.DEBUG)
 
-@app.route('/pred')
-@app.route('/')
+@app.route('/pred', methods=['POST'])
 
 def pred():
 
-    #data1 = pd.DataFrame.from_dict(data, orient="index")
-    #file = open('/home/patrick/Downloads/dump.txt', 'rb')
     app.logger.info('Processing default request')
     data_json = request.get_json()
 
     print("Loaded Json")
-    #data_dict = p.load(file)
-    data_dict = pd.read_json(data_json)
-    #print(data_dict.transpose())
-    print(data_dict)
-    prediction = model.predict(data_dict)
+    data_pd = pd.json_normalize(data_json)
+    prediction = model.predict(data_pd)
 
-    return prediction
+    return str(prediction)
 
 if __name__ == '__main__':
+    '''
     model_location = '/home/patrick/coding/ML-NIDS/network_intrusion.pickle'
     model = p.load(open(model_location, 'rb'))
-    #test = pred()
-    #print(test)
+    '''
+    from keras.models import load_model
+    model = load_model('/home/patrick/coding/autoencoder_model.h5')
     app.run()
